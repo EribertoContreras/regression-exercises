5d75a525dfe39471ef0ca9ba81e75dde1bbb84f2
Eriberto Contreras <eriberto.contreras91@gmail.com>
2022-05-31 15:46:46 -0500

    1

--- wrangle.py
+++ wrangle.py
@@ -1,4 +1,8 @@
+" In my wrangle py"
+
+
 from cgi import test
+from lib2to3.pgen2.pgen import DFAState
 import numpy as np
 import seaborn as sns
 import scipy.stats as stats
@@ -18,11 +22,7 @@ warnings.filterwarnings('ignore')
 def get_connection(db, user=env.user, host=env.host, password=env.password):
     return f'mysql+pymysql://{user}:{password}@{host}/{db}'
 
-def get_zillow_data():
-    return pd.read_sql('SELECT * FROM zillow', get_connection('zillow'))
-
- 
-# ------------------------------------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
 def get_zillow_data():
     """bedroomcnt, bathroomcnt, calculatedfinishedsquarefeet, taxvaluedollarcnt, yearbuilt, taxamount, and fips"""
     filename = "zillow.csv"
@@ -31,9 +31,8 @@ def get_zillow_data():
         return pd.read_csv(filename)
     else:
         # read the SQL query into a dataframe
-        df = pd.read_sql("""SELECT 
-             parcelid,
-			 bedroomcnt,
+        df = pd.read_sql("""SELECT parcelid,
+             bedroomcnt,
              bathroomcnt,
              calculatedfinishedsquarefeet,
              taxvaluedollarcnt,
@@ -49,28 +48,22 @@ def get_zillow_data():
         #changing it csv because its a csv
 
         # Return the dataframe to the calling code
-        return df  
+        return df   
 
 
 
 
-#def clean_zillow(df):
-
-   #  '''Prepares acquired zillow data for exploration'''
-    
-   # drop column using .drop(columns=column_name)
-    #df = df.drop(columns='species_id')
-    
-  # remame column using .rename(columns={current_column_name : replacement_column_name})
-    #df = df.rename(columns={'species_name':'species'})
-    
-    # create dummies dataframe using .get_dummies(column_name,not dropping any of the dummy columns)
-   #dummy_df = pd.get_dummies(df['species'], drop_first=False)
-    
-    # join original df with dummies df using .concat([original_df,dummy_df], join along the index)
-    #df = pd.concat([df, dummy_df], axis=1)
-    
-    #return df
+def clean_zillow(df):
+    """Prepares acquired zillow data for exploration"""
+    # Replace a whitespace sequence or empty with a NaN value and reassign this manipulation to df.
+    df = df.replace(r'^\s*$', np.nan, regex=True)
+    # Drop all rows with any Null values, assign to df, and verify.
+    df = df.dropna()
+    # Change all column data tyes to int64, reassign to df, and verify.
+    df = df.astype('int')
+    # getting rid of zero values in bedroomcnt and bathroomcnt
+    df = df[(df.bedroomcnt != 0) & (df.bathroomcnt != 0)]
+    return df
 
 
 # def split_zillow_data(df):
